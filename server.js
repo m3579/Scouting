8619328416c5ada79e0bb84c68fe6fbde674ec9a
mihir.kasmalkar <mihir.kasmalkar@warriorlife.net>
2017-03-14 21:58:16 -0400

    Added port to server code

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ var express = require("express");
 var app = express();
 var path = require("path");
 
+app.set("port", (process.env.PORT || 9000));
+
 app.set("views", __dirname + path.sep + "views");
 
 app.use(express.static(__dirname));
 
 app.listen(3000, function() {
-	console.log("Listening on port 3000");
+	console.log("Listening on port 9000");
 });
